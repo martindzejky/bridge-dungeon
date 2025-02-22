b781a0cdff7a3c1878a67f0f87939494af4d5aab
Martin Jakubik <martindzejky@gmail.com>
2025-02-22 22:18:22 +0100

    broken heart animation

---
+++ objects/broken-heart.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func onAnimationFinished(_anim_name):
+    queue_free()
--- ui/heart.gd
+++ ui/heart.gd
@@ -1,5 +1,14 @@
 extends TextureRect
 
+@export var brokenHeartScene: PackedScene
 
 func destroy():
     queue_free()
+
+    assert(brokenHeartScene, 'Broken heart scene missing!')
+
+    var broken := brokenHeartScene.instantiate()
+    var pos := get_global_transform_with_canvas().origin
+    broken.global_position = pos + Vector2(8, 8)
+
+    get_tree().call_group('ui-effects', 'add_child', broken)
